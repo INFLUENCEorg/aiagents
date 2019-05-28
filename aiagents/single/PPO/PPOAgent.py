a7e7afcbdd01c9c9233ea02666c5cb64d76c4913
czechows <czechows@users.noreply.github.com>
2019-05-28 13:46:27 +0200

    Changed some variables to private in PPO agent

---
+++ aiagents/single/PPO/PPOAgent.py
@@ -0,0 +1,197 @@
+from aiagents.single.PPO.controller import Controller
+from aiagents.single.PPO.PPOmodel import PPOmodel
+from aiagents.single.PPO.replay_memory import SerialSampling
+import numpy as np
+from aiagents.AgentComponent import AgentComponent
+import logging
+
+
+class PPOAgent(Controller, AgentComponent):
+
+    def observe(self, observation, reward, done):
+        new_state = np.zeros((1, self._parameters['frame_height'],
+                              self._parameters['frame_width'],
+                              self._parameters['num_frames']))
+        frame = observation
+        prev_state = np.copy(new_state)
+        new_state[0, :, :, 0] = frame
+        new_state[0, :, :, 1:] = prev_state[:, :, :, :-1]
+
+        next_step_output = dict({"obs": new_state, "reward": reward, "done": done, "prev_action": self._prev_action})
+        if( self._step_output["obs"] is None ):
+            self._step_output = next_step_output
+
+        self.increment_step()
+        get_actions_output = self._get_actions(self._step_output)
+
+        if self._parameters['mode'] == 'train':
+            # Store the transition in the replay memory.
+            self._add_to_memory(self._step_output,
+                                next_step_output,
+                                get_actions_output, self._t)
+            self._bootstrap(next_step_output)
+            if self._step % self._train_frequency == 0 and self._full_memory():
+                self._update()
+            if self._step % self._save_frequency == 0:
+                # Tensorflow only stores a limited number of networks.
+                self.save_graph(step)
+            self.write_summary()
+
+        self._step_output = next_step_output
+
+    def select_actions(self):
+        action=self._get_actions(self._step_output)
+        self._prev_action=action.get('action')[0]
+        return {self._controller_id: action['action'][0][0]}
+
+    def __init__(self, parameters, observation_space, action_space, controller_id=0, logger=None):
+        self._num_actions = {}
+        self._prev_state = None
+        self._observation_space = observation_space
+        #TODO: change to self._step_output = dict({"obs": observation_space.sample(), "reward": None, "done": None, "prev_action": None})
+        self._step_output = dict({"obs": None, "reward": None, "done": None, "prev_action": None})
+        self._prev_action = [-1]
+        self._parameters = parameters
+        self._num_actions = action_space.n
+        self._train_frequency = self._parameters['train_frequency']
+        self._save_frequency = self._parameters['save_frequency']
+        self._controller_id = controller_id
+        self._model = {}
+        self._replay_memory = {}
+        self._cumulative_rewards = {}
+        self._stats = {}
+        self._model = PPOmodel(self._parameters, self._num_actions)
+        self._replay_memory = SerialSampling(self._parameters, self._num_actions)
+        self._cumulative_rewards = 0
+        self._stats = {"cumulative_rewards": [],
+                        "episode_length": [],
+                        "value": [],
+                        "learning_rate": [],
+                        "entropy": [],
+                        "policy_loss": [],
+                        "value_loss": []}
+        super().__init__(self._parameters, action_space)
+        self._t = 0
+        if self._parameters['influence']:
+            self.seq_len = self._parameters['inf_seq_len']
+        elif self._parameters['recurrent']:
+            self.seq_len = self._parameters['seq_len']
+        else:
+            self.seq_len = 1
+
+
+    ############# PRIVATE METHODS ####################
+
+    def _add_to_memory(self, step_output, next_step_output, get_actions_output,
+                      episode_step):
+        """
+        Append the last transition to the replay memories of the factors.
+        """
+        # The given actions come from the experimentor and are actions,
+        # but we need action indices.
+        # TODO: this does not work as expected with factors. We would like to
+        # append the values of each factor to the corresponding key in the dictionary
+        self._replay_memory['obs'].append(step_output['obs'])
+        self._replay_memory['rewards'].append(next_step_output['reward'])
+        self._replay_memory['dones'].append(next_step_output['done'])
+        self._replay_memory['actions'].append(get_actions_output['action'])
+        self._replay_memory['values'].append(get_actions_output['value'][0])
+        self._replay_memory['action_probs'].append(get_actions_output['action_probs'])
+        # This mask is added so we can ignore experiences added when zero
+        # padding incomplete sequences
+        self._replay_memory['masks'].append(1)
+        self._cumulative_rewards += next_step_output['reward']
+        self._stats['value'].append(get_actions_output['value'][0])
+        self._stats['entropy'].append(get_actions_output['entropy'])
+        self._stats['learning_rate'].append(get_actions_output['learning_rate'])
+        # Note: States out is used when updating the network to feed the
+        # initial state of a sequence. In PPO this internal state will not
+        # differ that much from the current one. However for DQN we might
+        # rather set the initial state as zeros like in Jinke's
+        # implementation
+        if self._parameters['recurrent']:
+            self._replay_memory['states_in'].append(get_actions_output['state_in'])
+            self._replay_memory['prev_actions'].append(step_output['prev_action'])
+        if self._parameters['influence']:
+            self._replay_memory['inf_states_in'].append(get_actions_output['inf_state_in'])
+            self._replay_memory['inf_prev_actions'].append(step_output['prev_action'])
+
+        if next_step_output['done']:
+            if self._parameters['recurrent'] or self._parameters['influence']:
+                self._model.reset_state_in()
+            self._stats['cumulative_rewards'].append(self._cumulative_rewards)
+            self._stats['episode_length'].append(episode_step)
+            self._cumulative_rewards = 0
+            # zero padding incomplete sequences
+            remainder = len(self._replay_memory['masks']) % self.seq_len
+            if remainder != 0:
+                missing = self.seq_len - remainder
+                self._replay_memory.zero_padding(missing)
+                self._t += missing
+
+    def _bootstrap(self, next_step_output):
+        """
+        """
+        # TODO: consider the case where the episode is over because the maximum
+        # number of steps in an episode has been reached.
+        self._t += 1
+        if self._t >= self._parameters['time_horizon']:
+            evaluate_value_output = self._model.evaluate_value(
+                                        next_step_output['obs'],
+                                        next_step_output['prev_action'])
+            next_value = evaluate_value_output['value']
+            batch = self._replay_memory.get_last_entries(self._t, ['rewards', 'values', 'dones'])
+            advantages = self._compute_advantages(np.reshape(batch['rewards'],
+                                                            (1, -1)),
+                                                 np.reshape(batch['values'],
+                                                            (1, -1)),
+                                                 np.reshape(batch['dones'],
+                                                            (1, -1)),
+                                                 next_value,
+                                                 self._parameters['gamma'],
+                                                 self._parameters['lambda'])
+            self._replay_memory['advantages'].extend(advantages)
+            returns = advantages + batch['values']
+            self._replay_memory['returns'].extend(returns)
+            self._t = 0
+
+    def _update(self):
+        """
+        Sample a batch from the replay memory (if it is completely filled) and
+        use it to update the models.
+        """
+        import time
+        start = time.time()
+        logging.debug(len(self._replay_memory[0]['returns']))
+        logging.debug(len(self._replay_memory[0]['masks']))
+        policy_loss = 0
+        value_loss = 0
+        n_sequences = self._parameters['batch_size'] // self.seq_len
+        n_batches = self._parameters['memory_size'] // self._parameters['batch_size']
+        for e in range(self._parameters['num_epoch']):
+            self._replay_memory.shuffle()
+            for b in range(n_batches):
+                batch = self._replay_memory.sample(b, n_sequences)
+                update_model_output = self._model.update_model(batch)
+                policy_loss += update_model_output['policy_loss']
+                value_loss += update_model_output['value_loss']
+        self._replay_memory.empty()
+        self._stats['policy_loss'].append(np.mean(policy_loss))
+        self._stats['value_loss'].append(np.mean(value_loss))
+        end = time.time()
+        logging.debug(end - start)
+
+    def _compute_advantages(self, rewards, values, dones, last_value, gamma, lambd):
+        """
+        """
+        last_advantage = 0
+        advantages = np.zeros(self._parameters['time_horizon'], dtype=np.float32)
+        for t in reversed(range(self._parameters['time_horizon'])):
+            mask = 1.0 - dones[:, t]
+            last_value = last_value*mask
+            last_advantage = last_advantage*mask
+            delta = rewards[:, t] + gamma*last_value - values[:, t]
+            last_advantage = delta + gamma*lambd*last_advantage
+            advantages[t] = last_advantage
+            last_value = values[:, t]
+        return advantages
--- aiagents/single/PPO/controller.py
+++ aiagents/single/PPO/controller.py
@@ -9,7 +9,7 @@ class Controller(object):
     agent's actions, passes the environment signals to the agents.
     """
 
-    def __init__(self, parameters:dict, action_map, logger=None):
+    def __init__(self, parameters:dict, action_space, logger=None):
         """
         @param parameters a dictionary with all kinds of options for the run.
         @param action_map  a dict with 
@@ -17,30 +17,30 @@ class Controller(object):
         @param logger 
         """
         # Initialize all factor objects here
-        self.parameters = parameters
-        self.logger = logger
+        self._parameters = parameters
+        self._logger = logger
 
-        self.num_actions = {}
-        self.step = {}
+        self._num_actions = {}
+        self._step = {}
         tf.reset_default_graph()
-        self.num_actions = action_map.n
-        self.step = 0
-        summary_path = 'summaries/' + self.parameters['name'] + '_' + \
-                        self.parameters['algorithm']
+        self._num_actions = action_space.n
+        self._step = 0
+        summary_path = 'summaries/' + self._parameters['name'] + '_' + \
+                        self._parameters['algorithm']
         if not os.path.exists(summary_path):
             os.makedirs(summary_path)
-        self.summary_writer = tf.summary.FileWriter(summary_path)
+        self._summary_writer = tf.summary.FileWriter(summary_path)
 
-    def get_actions(self, step_output, ip=None):
+    def _get_actions(self, step_output, ip=None):
         """
         Get each factor's action based on its local observation. Append the given
         state to the factor's replay memory.
         """
         evaluate_policy_output = {}
-        evaluate_policy_output.update(self.model.evaluate_policy(step_output['obs'], step_output['prev_action']))
+        evaluate_policy_output.update(self._model.evaluate_policy(step_output['obs'], step_output['prev_action']))
         return evaluate_policy_output
 
-    def coordination(self, Q):
+    def _coordination(self, Q):
         """
         Runs coordination algorithm and returns the corresponding
         actions. Not using epsilon (for now) since it is used
@@ -53,48 +53,48 @@ class Controller(object):
         print(("Global action: {}".format(global_action)))
         return global_action
 
-    def get_last_transition_info(self):
+    def _get_last_transition_info(self):
         """
         Get each factor's TD-error given the transition information.
         """
         abs_td = []
-        batch = self.replay_memory.get_latest_entry()
-        td_error = self.model.td_error(batch)
+        batch = self._replay_memory.get_latest_entry()
+        td_error = self._model.td_error(batch)
         abs_td.append(np.abs(td_error))
 
         return abs_td
 
-    def update(self):
+    def _update(self):
         """
         Sample a batch from the replay memory (if it is completely filled) and
         use it to update the models.
         """
         raise NotImplementedError
 
-    def full_memory(self):
+    def _full_memory(self):
         """
         Check if the replay memories are filled.
         """
-        return self.replay_memory.full()
+        return self._replay_memory.full()
 
     def reset(self):
         """
         Reset the Q-functions. This is necessary for Recurrent Neural Networks.
         """
-        if not self.replay_memory.full():
-            print(("Replay Memory filled for {}%".format(self.replay_memory.fullness())))
+        if not self._replay_memory.full():
+            print(("Replay Memory filled for {}%".format(self._replay_memory.fullness())))
 
-        self.model.reset()
+        self._model.reset()
 
     def save_graph(self, time_step):
         """
         Store all the networks and replay memories.
         """
         # Create factor path if it does not exist.
-        factor_path = os.path.join('models', self.parameters['name'])
+        factor_path = os.path.join('models', self._parameters['name'])
         if not os.path.exists(factor_path):
             os.makedirs(factor_path)
-        self.model.save_graph(time_step)
+        self._model.save_graph(time_step)
 
     # TODO: replay memory
     def store_memory(self, path):
@@ -103,25 +103,25 @@ class Controller(object):
         if not os.path.exists(factor_path):
             os.makedirs(factor_path)
         # Store the replay memory
-        self.replay_memory.store(factor_path)
+        self._replay_memory.store(factor_path)
 
     def increment_step(self):
-        self.model.increment_step()
-        self.step = self.model.get_current_step()
+        self._model.increment_step()
+        self._step = self._model.get_current_step()
 
     def write_summary(self):
         """
         Saves training statistics to Tensorboard.
         """
-        if self.step % self.parameters['summary_frequency'] == 0 and \
-           self.parameters['tensorboard']:
+        if self._step % self._parameters['summary_frequency'] == 0 and \
+           self._parameters['tensorboard']:
 
             summary = tf.Summary()
             for key in self.stats.keys():
                 if len(self.stats[key]) > 0:
                     stat_mean = float(np.mean(self.stats[key]))
-                    #self.logger.log_scalar(key, stat_mean, self.step)
+                    #self._logger.log_scalar(key, stat_mean, self._step)
                     summary.value.add(tag='{}'.format(key), simple_value=stat_mean)
                     self.stats[key] = []
-            self.summary_writer.add_summary(summary, self.step)
-            self.summary_writer.flush()
+            self._summary_writer.add_summary(summary, self.step)
+            self._summary_writer.flush()
