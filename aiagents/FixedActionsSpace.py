a7f0fcf3e59f5a02dc3a310706115ff9f80f913e
wouter <wouter@tud206796>
2019-06-13 09:13:30 +0200

    #12 fix name

--- aiagents/FixedActionsSpace.py
+++ aiagents/FixedActionsSpace.py
@@ -1,16 +1,16 @@
 from gym.spaces.space import Space
 
 
-class FixedActionSpace(Space):
+class FixedActionsSpace(Space):
   """
-  FixedActionSpace assumes that there is a enumerable set 
+  FixedActionSpace assumes that there is an enumerable set 
   of all possible actions that can become possible in the environment,
   even if it would run infinitely long.
   """
 
   def getAllActions(self) -> dict:
       """
-      get the complete dict of all actions that might become
+      @return the complete dict of all actions that might become
       possible in this actionspace.
       the key is the int action number,
       the value a human-readable action value.
