84f692d0400302e9676db8d0c5f13ca7892c0ca5
azzmusam <34919691+azzmusam@users.noreply.github.com>
2019-07-01 12:55:23 +0200

    Add files via upload

---
+++ dqn/dqnmodel.py
@@ -0,0 +1,234 @@
+import os
+import numpy as np
+import tensorflow as tf
+import time
+
+class DeepQNetwork(object):
+
+    def __init__(self, lr, n_actions, name, fc1_dims=1024,
+                 input_dims=(210, 160, 4), chkpt_dir="tmp/dqn"):
+        self.lr = lr
+        self.name = name
+        self.n_actions = n_actions
+        self.fc1_dims = fc1_dims
+        self.chkpt_dir = chkpt_dir
+        self.input_dims = input_dims
+        self.sess = tf.Session()
+        self.build_network()
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
+        self.checkpoint_file = os.path.join(chkpt_dir, "deepqnet.ckpt")
+        self.params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                        scope=self.name)
+        self.write_op = tf.summary.merge([self.accuracy_sum, self.loss_sum, self.summ])
+        self.writer = tf.summary.FileWriter("tmp/log_dir")
+        self.writer.add_graph(self.sess.graph)
+
+
+        # The list of values in the collection with the given name
+        # or an empty list if no value has been added to that collection.
+        # trainable variables are the whose values are updated while performing optimisation.
+
+    def build_network(self):
+        with tf.variable_scope(self.name):
+            self.input = tf.placeholder(tf.float32, shape=[None, *self.input_dims],
+                                        name='inputs')
+            # * here indicates that the function can take multiple inputs as arguments into the function.
+            self.actions = tf.placeholder(tf.float32, shape=[None, self.n_actions],
+                                         name='action_taken')
+            self.q_target = tf.placeholder(tf.float32, shape=[None, self.n_actions],
+                                           name='q_target')
+
+            # 1st dimension inside shape is set to None because we want to pass
+            # batches of stacked frame into the neural network.
+
+            conv1 = tf.layers.conv2d(inputs=self.input, filters=32,
+                                     kernel_size=(8, 8), strides=4, name='conv1',
+                                     kernel_initializer=tf.contrib.layers.variance_scaling_initializer(factor=2))
+            conv1_activated = tf.nn.relu(conv1)
+
+
+            conv2 = tf.layers.conv2d(inputs=conv1_activated, filters=64,
+                                     kernel_size=(4, 4), strides=2, name='conv2',
+                                     kernel_initializer=tf.contrib.layers.variance_scaling_initializer(factor=2))
+            conv2_activated = tf.nn.relu(conv2)
+
+
+            conv3 = tf.layers.conv2d(inputs=conv2_activated, filters=128,
+                                     kernel_size=(3, 3), strides=1, name='conv3',
+                                     kernel_initializer=tf.contrib.layers.variance_scaling_initializer(factor=2))
+            conv3_activated = tf.nn.relu(conv3)
+
+            flat = tf.contrib.layers.flatten(conv3_activated)
+
+            dense1 = tf.layers.dense(flat, units=self.fc1_dims, activation=tf.nn.relu,
+                                     kernel_initializer=tf.contrib.layers.variance_scaling_initializer(factor=2))
+
+            self.Q_values = tf.layers.dense(dense1, units=self.n_actions,
+                                            kernel_initializer=tf.contrib.layers.variance_scaling_initializer(factor=2))
+
+
+            self.q = tf.reduce_sum(tf.multiply(self.Q_values, self.actions))
+            self.accuracy_sum = tf.summary.scalar('Accuracy', self.q)
+
+            self.loss = tf.reduce_mean(tf.square(self.q - self.q_target))
+            self.loss_sum = tf.summary.scalar("Loss", self.loss)
+
+            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+            for var in tf.trainable_variables():
+                print(var.name[:-2])
+                self.summ = tf.summary.histogram(var.name[:-2], var)
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.saver.restore(self.sess, self.checkpoint_file)
+
+    def save_checkpoint(self):
+        print('... Saving Checkpoint ...')
+        self.saver.save(self.sess, self.checkpoint_file)
+
+class Agent(object):
+    def __init__(self, alpha, gamma, mem_size, n_actions, epsilon, batch_size,
+                 replace_target=2, input_dims=(210, 160, 4),
+                 q_next_dir="tmp/q_next", q_eval_dir="tmp/q_eval"):
+        self.n_actions = n_actions
+        self.action_space = [i for i in range(self.n_actions)]
+        # for n_actions=3, action_space is a list [0, 1, 2]
+        self.gamma = gamma
+        self.mem_size = mem_size
+        self.mem_cntr = 0
+        self.epsilon = epsilon
+        self.batch_size = batch_size
+        self.replace_target = replace_target
+        self.q_next = DeepQNetwork(alpha, n_actions, input_dims=input_dims,
+                                   name='q_next', chkpt_dir=q_next_dir)
+        self.q_eval = DeepQNetwork(alpha, n_actions, input_dims=input_dims,
+                                   name='q_eval', chkpt_dir=q_eval_dir)
+
+        # Defining arrays of zeros for state, rewards etc to be stored.
+        self.state_memory = np.zeros((self.mem_size, *input_dims))
+        # will save a set of 4 stacked frames by number of memories.
+        # state_memory has shape(mem_size, 210, 160, 4).
+
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims))
+        self.action_memory = np.zeros((self.mem_size, self.n_actions),
+                                      dtype=np.int8)
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.int8)
+        # terminal only has 0 and 1 as input. At end of an episode
+        # shape of terminal_memory is (mem_size, 0) with all elements being 0.
+        # we don't to have future rewards, this will be indicated by the terminal memory.
+
+
+    def store_transition(self, state, action, reward, state_, terminal):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        actions = np.zeros(self.n_actions)
+        # actions are arrays of zeros(vector) with len equal to the number of actions. [0, 0, 0]
+        # action_space is a list of len equal to the  number of action [0, 1, 2]
+        actions[action] = 1.0
+        # the action which is given as index is the action chosen.
+        # This will assign integer 1 to the action that has ben selected. This is like One hot encoding.
+        self.action_memory[index] = actions
+        self.reward_memory[index] = reward
+        # shape (mem_size,)
+        self.new_state_memory[index] = state_
+        self.terminal_memory[index] = terminal
+        # It is a done flag, 0 for done and 1 for not done. It indicates the completion of an episode.
+        self.mem_cntr += 1
+
+    def choose_action(self, state):
+        rand = np.random.random()
+        if rand < self.epsilon:
+            action = np.random.choice(self.action_space)
+            # since action_space has actions as [0, 1, 2], it will generate an integer from elements of action space.
+            # That is either 0th, 1st or 2nd action.
+        else:
+            actions = self.q_eval.sess.run(self.q_eval.Q_values,
+                                           feed_dict={self.q_eval.input: state})
+            action = np.argmax(actions)
+
+        return action
+
+    def learn(self):
+        if self.mem_cntr % self.replace_target == 0:
+            self.update_graph()
+        # we update the graph after every K steps, so that the q_target is not fluctuating.
+
+        max_mem = self.mem_cntr if self.mem_cntr < self.mem_size else self.mem_size
+
+        batch = np.random.choice(max_mem, self.batch_size)
+        # Batch is of the length equal to batch size with elements that are generated using np.arange(max_mem) (b).
+
+        state_batch = self.state_memory[batch]
+        # Shape of the state batch is equal to (batch_size, input_dims)
+        # ex: (32, 180, 160, 4)
+
+        action_batch = self.action_memory[batch]
+        action_values = np.array([0, 1, 2], dtype=np.int8)
+        action_indices = np.dot(action_batch, action_values)
+        reward_batch = self.reward_memory[batch]
+        new_state_batch = self.new_state_memory[batch]
+        terminal_batch = self.terminal_memory[batch]
+
+        q_eval = self.q_eval.sess.run(self.q_eval.Q_values,
+                                      feed_dict={self.q_eval.input: state_batch})
+        # It has shape (batch_size, n_actions).
+        # This gives Q values for each action, in this case 3 actions, using q_eval network for current state batch.
+
+        q_next = self.q_next.sess.run(self.q_next.Q_values,
+                                      feed_dict={self.q_next.input: new_state_batch})
+        # This gives Q values for the next state using the q_next network.
+
+        q_target = q_eval.copy()
+        idx = np.arange(self.batch_size)
+        q_target[idx, action_indices] = reward_batch + \
+            self.gamma*np.max(q_next, axis=1)*terminal_batch
+        # axis= 1 means along each row we calculate the maximum value, where rows are the actions.
+
+        #q_target = np.zeros(self.batch_size)
+        #q_target = reward_batch + self.gamma*np.max(q_next, axis =1)*terminal_batch
+
+        _ = self.q_eval.sess.run(self.q_eval.train_op,
+                                 feed_dict={self.q_eval.input: state_batch,
+                                            self.q_eval.actions: action_batch,
+                                            self.q_eval.q_target: q_target})
+
+        loss = self.q_eval.sess.run(self.q_eval.loss,
+                                    feed_dict={self.q_eval.input: state_batch,
+                                               self.q_eval.actions: action_batch,
+                                               self.q_eval.q_target: q_target})
+
+
+
+        summary = self.q_eval.sess.run(self.q_eval.write_op,
+                                       feed_dict={self.q_eval.input: state_batch,
+                                                  self.q_eval.actions: action_batch,
+                                                  self.q_eval.q_target: q_target,
+                                                  self.q_next.input: new_state_batch})
+
+        self.q_eval.writer.add_summary(summary, time.time())
+        self.q_eval.writer.flush()
+
+
+        if self.mem_cntr > 1000:
+            if self.epsilon > 0.05:
+                self.epsilon -= 4e-7
+            elif self.epsilon <= 0.05:
+                self.epsilon = 0.05
+
+    def save_models(self):
+        self.q_eval.save_checkpoint()
+        self.q_next.save_checkpoint()
+
+    def load_models(self):
+        self.q_eval.load_checkpoint()
+        self.q_next.load_checkpoint()
+
+    def update_graph(self):
+        t_params = self.q_next.params
+        e_params = self.q_eval.params
+
+        for t, e in zip(t_params, e_params):
+            self.q_eval.sess.run(tf.assign(t, e))
--- dqn/main.py
+++ dqn/main.py
@@ -1,38 +1,52 @@
-import gym
-from dqn import DeepQNetwork, Agent
+import sys
+sys.path
+from dqnmodel import DeepQNetwork, Agent
 from utils import plotLearning
 import numpy as np
+import gym
+import gym_gridworlds
 import os
-from gym import wrappers
-import matplotlib.pyplot as plt
+
 
 def preprocess(observation):
     return np.mean(observation[30:, :], axis=2).reshape(180, 160, 1)
+    # cropping the image, therefore considering only after 30th row of the array.
+    # Taking mean along the axis=2 which is the axis representing the colour of
+    # of the frames, so we reduce it from RBG to one containing the mean, with only
+    # single channel.
 
 def stack_frames(stacked_frames, frame, buffer_size):
     if stacked_frames is None:
         stacked_frames = np.zeros((buffer_size, *frame.shape))
+        # here frame.shape has the shape of observation after pre-processing (180, 160, 1).
         for idx, _ in enumerate(stacked_frames):
             stacked_frames[idx, :] = frame
+            # stacked_frames[idx, :].shape = (180, 160, 1)
     else:
         stacked_frames[0:buffer_size-1, :] = stacked_frames[1:, :]
+        # Removing the first frame in order to store the current new frame
         stacked_frames[buffer_size-1, :] = frame
+        # Adding the current frame to the stacked_frames.
 
     stacked_frames = stacked_frames.reshape(1, *frame.shape[0:2], buffer_size)
+    # stacked_frames.shape=(1, 180, 160, 4) after reshaping.
+    # frame.shape=(2, 160, 1)
 
     return stacked_frames
 
 
 if __name__ == '__main__':
-    env = gym.make('Breakout-v0')
+
+    env = gym.make('Gridworld-v0')
     load_checkpoint = os.path.isfile('tmp/q_eval/deepqnet.ckpt')
     agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.00025, input_dims=(180,160,4),
-                  n_actions=3, mem_size=2000, batch_size=32)
+                  n_actions=4, mem_size=1000, batch_size=32)
     if load_checkpoint:
         agent.load_models()
     filename = 'breakout-alpha0p000025-gamma0p99-only-one-fc.png'
     scores = []
     eps_history = []
+    episode_loss = []
     numGames = 5000
     stack_size = 4
     score = 0
@@ -42,40 +56,68 @@ if __name__ == '__main__':
 
     print("Loading up the agent's memory with random gameplay")
 
-    while agent.mem_cntr < 2000:
+    while agent.mem_cntr < 1000:
         done = False
         observation = env.reset()
+        # observation.shape = (210, 160, 3)
         observation = preprocess(observation)
+        # now the shape is (180, 160, 1)
         stacked_frames = None
         observation = stack_frames(stacked_frames, observation, stack_size)
+        # The shape changes to (1, 180, 160, 4)
         while not done:
             action = np.random.choice([0, 1, 2])
+            # Randomly choose a single element from the array above and
+            # assign it to the variable action.
             action += 1
+            # we add one to the action and pass it to the env.step()
+            # so if 0th action is chosen, adding one makes it one and then passing
+            # 1 into the env.step(1) selects first action, which is actually the 0th action.
+
             observation_, reward, done, info = env.step(action)
+            # observation_.shape = (210, 160, 3)
+            # info is {'ale.lives':5}
+
             observation_ = stack_frames(stacked_frames,
                                         preprocess(observation_), stack_size)
+            # observation_.shape =(1, 180, 160,4)
             action -= 1
+            # Then we subtract 1 back since we added it to the action.
             agent.store_transition(observation, action,
                                    reward, observation_, int(done))
+            # Then this is passed to the store.transition() function which first creates an
+            # array of zeros(called actions) like ([0, 0, 0]) with len equal to the number
+            # of actions.  This action is passed as an index to this array of zeros and
+            # that particular element is replaced by one. So if 0th action is selected,
+            # the ac
+            # [1, 0, 0]). This is hot encoding the action
+
             observation = observation_
+            # observation.shape = (1, 180, 160, 4)
     print("Done with random game play. Game on.")
 
     for i in range(numGames):
+       # if(i==1):
+      #      pdb.set_trace()
         done = False
-        if i % 10 == 0 and i > 0:
-            avg_score = np.mean(scores[max(0, i-10):(i+1)])
+        if i % 2 == 0 and i > 0:
+            #avg_score = np.mean(scores[max(0, i-10):(i+1)])
             print('episode: ', i, 'score: ', score,
-                  ' average score %.3f' % avg_score,
+                  #' average score %.3f' % avg_score,
                   'epsilon %.3f' % agent.epsilon)
-            agent.save_models()
+
         else:
             print('episode: ', i, 'score: ', score)
 
         observation = env.reset()
         observation = preprocess(observation)
         stacked_frames = None
+        # we change it to none inside the loop so that we can have current 4 stacked of frames.
+
         observation = stack_frames(stacked_frames, observation, stack_size)
+
         score = 0
+
         while not done:
             action = agent.choose_action(observation)
             action += 1
