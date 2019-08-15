b4658033901613e1b2e03ac99cd6c83f9badf9b6
wouter <wouter@tud206796>
2019-08-15 10:24:55 +0200

    #117 mcts agent drop simulator from constructor and use the deepcopy

--- aiagents/single/mcts/mctsAgent.py
+++ aiagents/single/mcts/mctsAgent.py
@@ -7,10 +7,11 @@ from aiagents.multi.ComplexAgentComponent import BasicComplexAgent
 from aienvs.Environment import Env
 import math
 
+
 class MctsAgent():
     DEFAULT_PARAMETERS = {'treeParameters': {'explorationConstant': 1 / math.sqrt(2), 'samplingLimit': 20}}
 
-    def __init__(self, agentId, environment: Env, parameters: dict, otherAgents=None, simulator=None, treeAgent=None, rolloutAgent=None): 
+    def __init__(self, agentId, environment: Env, parameters: dict, otherAgents=None, treeAgent=None, rolloutAgent=None): 
         """
         TBA
         """
@@ -24,26 +25,23 @@ class MctsAgent():
             self._limitType = 'time'
         else:
             if 'iterationLimit' not in self._parameters:
-                DEFAULT_LIMIT=1000
+                DEFAULT_LIMIT = 1000
                 logging.error("Must have either a time limit or an iteration limit. Using default iteration limit: " + str(DEFAULT_LIMIT))
-                self._parameters['iterationLimit']=DEFAULT_LIMIT
+                self._parameters['iterationLimit'] = DEFAULT_LIMIT
             # number of iterations of the search
             if self._parameters['iterationLimit'] < 1:
                 raise ValueError("Iteration limit must be greater than one")
             self._limitType = 'iterations'
 
-        if simulator is None:
-            self._simulator = copy.deepcopy(environment)
-        else:
-            self._simulator = simulator
+        self._simulator = copy.deepcopy(environment)
 
         if treeAgent is None:
-            self._treeAgent = RandomAgent( self.agentId, self._simulator )
+            self._treeAgent = RandomAgent(self.agentId, self._simulator)
         else:
             self._treeAgent = treeAgent
 
         if rolloutAgent is None:
-            self._rolloutAgent = RandomAgent( self.agentId, self._simulator )
+            self._rolloutAgent = RandomAgent(self.agentId, self._simulator)
         else:
             self._rolloutAgent = rolloutAgent
             
@@ -54,9 +52,9 @@ class MctsAgent():
             # whatever action is ok
             return self._treeAgent.step(observation, reward, done)
 
-        root = RootNode(state=observation, reward=0., done=done, simulator=self._simulator, 
+        root = RootNode(state=observation, reward=0., done=done, simulator=self._simulator,
                 agentId=self.agentId, parameters=self._parameters['treeParameters'],
-                treeAgent=self._treeAgent, otherAgents=self._otherAgents, rolloutAgent = self._rolloutAgent)
+                treeAgent=self._treeAgent, otherAgents=self._otherAgents, rolloutAgent=self._rolloutAgent)
 
         if self._limitType == 'time':
             timeLimit = time.time() + self._parameters['timeLimit']
@@ -67,7 +65,7 @@ class MctsAgent():
                 root.executeRound()
 
         action = root.getBestChild().getAction()
-        logging.info("Action "+ str(action))
+        logging.info("Action " + str(action))
 
         return action
 
