596bc45c70f907091a407e84a4c4a3eaa5bea3e7
wouter <wouter@tud206796>
2019-10-29 10:06:26 +0100

    fix ComplexAgent docu

--- aiagents/multi/ComplexAgent.py
+++ aiagents/multi/ComplexAgent.py
@@ -9,7 +9,13 @@ class ComplexAgent(AgentComponent):
     This is abstract because step is not implemented at this point.
     """
 
-    def __init__(self, agentComponentList, environment:Env=None, parameters=None):
+    def __init__(self, agentComponentList:list, environment:Env=None, parameters=None):
+        """
+        @param agentComponentList list of AgentComponent's children of this 
+        @param environment the environment that this component and children work in
+        @param parameters the additional setup parameters, used for
+        this only (not for the children). 
+        """
         self._agentSubcomponents = agentComponentList
         self._environment = environment
 
