4905e4cfcca4ed9f1f07afa54281d57a5eab7063
wouter <wouter@tud206796>
2019-10-31 14:27:15 +0100

    #6 more tests added. No full coverage yet but continuing with integration test as requested by Alex

--- test/single/testQAgent.py
+++ test/single/testQAgent.py
@@ -41,3 +41,28 @@ class testQAgent(LoggedTestCase):
         agent._updateQ(STATE2, ACT2, STATE3, 2)
         agent._updateQ(STATE2, ACT2, STATE3, 2)
         self.assertEqual(0.9 * 0.2 + 0.1 * 2, agent._getQ(STATE2, ACT2))
+
+    def test_getMaxQ(self):
+        agent = QAgent("agent1", Mock(), {'alpha':0.1, 'gamma':1, 'm':500, 's':0.01})
+        agent._updateQ(STATE1, ACT1, STATE2, 2)
+        agent._updateQ(STATE1, ACT2, STATE3, 3)
+        self.assertEqual(0.1 * 3, agent._getMaxQ(STATE1))
+        self.assertEqual(ACT2, agent._getMaxAction(STATE1))
+    
+    def test_chooseActionBest(self):
+        # -500,1 results in p=1 everywhere so always choose BEST action
+        agent = QAgent("agent1", Mock(), {'alpha':0.1, 'gamma':1, 'm':-500, 's':1})
+        agent._updateQ(STATE1, ACT1, STATE2, 2)
+        agent._updateQ(STATE1, ACT2, STATE3, 3)
+        for i in range(10):
+            # chooseaction may act randomly. test multiple times.
+            self.assertEqual(ACT2, agent._chooseAction(STATE1))
+        
+    def test_chooseActionRandom(self):
+        # 500,1 results in p=0 everywhere so always choose RANDOM action
+        agent = QAgent("agent1", Mock(), {'alpha':0.1, 'gamma':1, 'm':500, 's':1})
+        agent._updateQ(STATE1, ACT1, STATE2, 2)
+        agent._updateQ(STATE1, ACT2, STATE3, 3)
+        chosen = [agent._chooseAction(STATE1) for x in range(100)] 
+        
+        
