7d089f5042c2471c8381d95be12fab1db78d3760
wouter <wouter@tud206796>
2020-06-04 11:27:52 +0200

    #19 fix AgentFactory and factory_floor_complex.yaml which I guessed was the correct definition file

--- examples/AgentFactory.py
+++ examples/AgentFactory.py
@@ -1,31 +1,31 @@
 from aiagents.AgentFactory import createAgent
-from mock import Mock
+from unittest.mock import Mock
 from aienvs.FactoryFloor.FactoryFloor import FactoryFloor
 from aienvs.utils import getParameters
 import sys
 import os
 
 
-
 def main():
     """
-    Demo how to construct agents
+    Demo how to construct complex agents
     """
-    configName = "configs/agentconfig.yaml"
+    configName = "configs/factory_floor_complex.yaml"
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, configName)
 
-    parameters=getParameters(filename)
+    parameters = getParameters(filename)
  
     env = FactoryFloor(parameters['environment'])
     obs = env.reset()
 
-    complexAgent = createAgent(env, parameters['agents'])
+    complexAgent = createAgent(env.action_space, env.observation_space, parameters['agents'])
     print(complexAgent)
-    print(complexAgent._agentSubcomponents)
-    print(complexAgent._agentSubcomponents[2]._agentSubcomponents)
+    sub = complexAgent._agentSubcomponents[0]
+    subsub = sub._agentSubcomponents
+    print(subsub)
+
 	
 if __name__ == "__main__":
         main()
-	
 
