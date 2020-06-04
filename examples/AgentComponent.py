51d77a339618ba4c83fe20e22b402ea384bfa19f
wouter <wouter@tud206796>
2020-06-04 09:55:49 +0200

    #19 fix AgentComponent example

--- examples/AgentComponent.py
+++ examples/AgentComponent.py
@@ -1,33 +1,36 @@
 from aiagents.multi.BasicComplexAgent import BasicComplexAgent
 from aiagents.single.RandomAgent import RandomAgent
-from gym.spaces import Discrete
+from gym.spaces import Discrete, Dict
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+
 def main():
 
-    N_agents=10
-    i=0
-    action_space=Discrete(3)
-    simpleComponentList=[]
+    N_agents = 10
+    i = 0
+    action_space = Dict({ n: Discrete(3) for n in range(N_agents) })
+    obs_space = None  # not used anyway in this demo
+    simpleComponentList = []
 
-    while( i < N_agents ):
-        simpleComponentList.append(RandomAgent(i, action_space))
-        i+=1
+    while(i < N_agents):
+        simpleComponentList.append(RandomAgent(i, action_space, obs_space))
+        i += 1
 
-    myComplexComponent=BasicComplexAgent(simpleComponentList)
+    myComplexComponent = BasicComplexAgent(simpleComponentList, action_space, obs_space)
 
-    N_steps=3
-    i=0
-    state=0
+    N_steps = 3
+    i = 0
+    state = 0
 
-    while i<N_steps:
+    while i < N_steps:
         myComplexComponent.step(state)
-        i+=1
+        i += 1
     return 0
+
   
-if __name__== "__main__":
+if __name__ == "__main__":
   main()
 
